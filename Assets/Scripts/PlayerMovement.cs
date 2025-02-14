241ab2f6f032933fe00c3b32dffa5de6a374c61b
alerdd <g.igumentsev968@gmail.com>
2025-02-14 22:23:47 +0500

    v0.2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,48 @@
-using UnityEngine;
+ using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D body;
-    [SerializeField] private float speed;
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    private Rigidbody2D rb;
+    private Animator anim;
 
-    private void Awake()
+    private int jumpCount = 0;
+    public int maxJumpCount = 2;
+
+    void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody2D component is missing from this GameObject. Please add a Rigidbody2D.");
+        }
     }
 
-
-    private void Update()
+    void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"),body.velocity.y);
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
 
-        if(Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x,speed);
-    }   
+        if (moveHorizontal > 0.01f)
+            transform.localScale = Vector3.one;
+        if (moveHorizontal < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumpCount)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount++;
+        }
+
+        if (rb.velocity.y == 0)
+        {
+            jumpCount = 0;
+        }
+
+        anim.SetBool("Run",moveHorizontal != 0);
+    }
 }
